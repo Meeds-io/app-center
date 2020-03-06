328464d54ef455159a5153adae2d5d1f545f9cbd
Boubaker Khanfir <bkhanfir@exoplatform.com>
2020-03-06 08:56:57 +0100

    Add more javadoc

--- app-center-services/src/main/java/org/exoplatform/appcenter/service/ApplicationCenterService.java
+++ app-center-services/src/main/java/org/exoplatform/appcenter/service/ApplicationCenterService.java
@@ -137,6 +137,15 @@ public class ApplicationCenterService {
     return appCenterStorage.updateApplication(application);
   }
 
+  /**
+   * Delete application identified by its id and check if username has
+   * permission to delete it.
+   * 
+   * @param applicationId technical identifier of application
+   * @param username user currently deleting application
+   * @throws ApplicationNotFoundException if application wasn't found
+   * @throws IllegalAccessException if user is not allowed to delete application
+   */
   public void deleteApplication(Long applicationId, String username) throws ApplicationNotFoundException, IllegalAccessException {
     if (applicationId == null || applicationId <= 0) {
       throw new IllegalArgumentException("applicationId must be a positive integer");
@@ -185,6 +194,13 @@ public class ApplicationCenterService {
     appCenterStorage.addApplicationToUserFavorite(applicationId, username);
   }
 
+  /**
+   * Deletes an application identified by its id from favorite applications of
+   * user
+   * 
+   * @param applicationId application technical identifier
+   * @param username login of user currently deleting application
+   */
   public void deleteFavoriteApplication(Long applicationId, String username) {
     if (applicationId == null || applicationId <= 0) {
       throw new IllegalArgumentException("applicationId must be a positive integer");
@@ -195,13 +211,18 @@ public class ApplicationCenterService {
     appCenterStorage.deleteApplicationFavorite(applicationId, username);
   }
 
-  public void setMaxFavoriteApps(long number) {
-    if (number > 0) {
+  /**
+   * Change general setting for maximum allowed favorites that a user can have
+   * 
+   * @param maxFavoriteApplications max favorite applications count
+   */
+  public void setMaxFavoriteApps(long maxFavoriteApplications) {
+    if (maxFavoriteApplications > 0) {
       settingService.set(APP_CENTER_CONTEXT,
                          APP_CENTER_SCOPE,
                          MAX_FAVORITE_APPS,
-                         SettingValue.create(number));
-      this.maxFavoriteApps = number;
+                         SettingValue.create(maxFavoriteApplications));
+      this.maxFavoriteApps = maxFavoriteApplications;
     } else {
       settingService.remove(APP_CENTER_CONTEXT,
                             APP_CENTER_SCOPE,
@@ -210,6 +231,9 @@ public class ApplicationCenterService {
     }
   }
 
+  /**
+   * @return the maximum favorite applications that a user can have as favorite
+   */
   public long getMaxFavoriteApps() {
     if (this.maxFavoriteApps < 0) {
       SettingValue<?> maxFavoriteAppsValue = settingService.get(APP_CENTER_CONTEXT, APP_CENTER_SCOPE, MAX_FAVORITE_APPS);
@@ -222,6 +246,13 @@ public class ApplicationCenterService {
     return this.maxFavoriteApps;
   }
 
+  /**
+   * Stores default image for applications not having an attached illustration
+   * 
+   * @param defaultAppImage image content and name
+   * @return stored image
+   * @throws Exception if an exception occurs while storing image into database
+   */
   public ApplicationImage setDefaultAppImage(ApplicationImage defaultAppImage) throws Exception {
     if (defaultAppImage == null
         || (StringUtils.isBlank(defaultAppImage.getFileName()) && StringUtils.isBlank(defaultAppImage.getFileBody()))) {
@@ -241,6 +272,12 @@ public class ApplicationCenterService {
     return null;
   }
 
+  /**
+   * @return {@link GeneralSettings} of application including default image and
+   *         maximum favorite applications count
+   * @throws Exception if an exception occurs while retrieving image data from
+   *           store
+   */
   public GeneralSettings getAppGeneralSettings() throws Exception { // NOSONAR
     GeneralSettings generalsettings = new GeneralSettings();
     generalsettings.setMaxFavoriteApps(getMaxFavoriteApps());
@@ -253,6 +290,16 @@ public class ApplicationCenterService {
     return generalsettings;
   }
 
+  /**
+   * Retrieves the list of applications with offset, limit and a keyword that
+   * can be empty
+   * 
+   * @param offset offset of the query
+   * @param limit limit of the query that can be less or equal to 0, which mean,
+   *          getting all available applications
+   * @param keyword used to search in title and url
+   * @return {@link ApplicationList} that contains the list of applications
+   */
   public ApplicationList getApplicationsList(int offset,
                                              int limit,
                                              String keyword) {
@@ -264,6 +311,19 @@ public class ApplicationCenterService {
     return applicationList;
   }
 
+  /**
+   * Retrieves the list of applications switch offset and limit of the query, a
+   * keyword to filter on title and url of {@link Application} and the username
+   * to filter on authorized applications
+   * 
+   * @param offset offset of the query
+   * @param limit limit of the query that can be less or equal to 0, which mean,
+   *          getting all available applications
+   * @param keyword used to search in title and url
+   * @param username login of user to use to filter on authorized applications
+   * @return {@link ApplicationList} that contains the {@link List} of
+   *         authorized {@link UserApplication}
+   */
   public ApplicationList getAuthorizedApplicationsList(int offset,
                                                        int limit,
                                                        String keyword,
@@ -285,11 +345,30 @@ public class ApplicationCenterService {
     return resultApplicationsList;
   }
 
+  /**
+   * Retrieves all the list of applications for a user
+   * 
+   * @param username login of user
+   * @return {@link List} of {@link UserApplication}
+   */
   public List<UserApplication> getFavoriteApplicationsList(String username) {
     List<UserApplication> favoriteApplications = appCenterStorage.getFavoriteApplicationsByUser(username);
     return favoriteApplications.stream().filter(app -> hasPermission(username, app)).collect(Collectors.toList());
   }
 
+  /**
+   * Return the {@link Application} illustration last modifed timestamp (in ms),
+   * if not found, the default image last modifed timestamp will be retrieved
+   * 
+   * @param applicationId technical id of application
+   * @param username login of user accessing application
+   * @return timestamp in milliseconds of last modified date of illustration
+   * @throws ApplicationNotFoundException if application wasn't found
+   * @throws IllegalAccessException if user doesn't have access permission to
+   *           application
+   * @throws FileStorageException if an error occurs while accessing file from
+   *           store
+   */
   public Long getApplicationImageLastUpdated(long applicationId, String username) throws ApplicationNotFoundException,
                                                                                   IllegalAccessException,
                                                                                   FileStorageException {
@@ -311,6 +390,20 @@ public class ApplicationCenterService {
     return null;
   }
 
+  /**
+   * Return the {@link Application} illustration {@link InputStream}, if not
+   * found, the default image {@link InputStream} will be retrieved
+   * 
+   * @param applicationId technical id of application
+   * @param username login of user accessing application
+   * @return {@link InputStream} of application illustration
+   * @throws ApplicationNotFoundException if application wasn't found
+   * @throws IllegalAccessException if user doesn't have access permission to
+   *           application
+   * @throws FileStorageException if an error occurs while accessing file from
+   *           store
+   * @throws IOException if an error occurs while building {@link InputStream}
+   */
   public InputStream getApplicationImageInputStream(long applicationId, String username) throws ApplicationNotFoundException,
                                                                                          IllegalAccessException,
                                                                                          FileStorageException,
@@ -359,17 +452,25 @@ public class ApplicationCenterService {
       return true;
     }
 
-    org.exoplatform.services.security.Identity identity = identityRegistry.getIdentity(username);
+    // Ingeneral case, the user is already loggedin, thus we will get the
+    // Identity from registry without having to compute it again from
+    // OrganisationService, thus the condition (identity == null) will be false
+    // most of the time for better performances
+    Identity identity = identityRegistry.getIdentity(username);
     if (identity == null) {
       try {
         identity = authenticator.createIdentity(username);
       } catch (Exception e) {
         LOG.warn("Error getting memberships of user {}", username, e);
+        return false;
+      }
+
+      // Check null again after building identity
+      if (identity == null) {
+        return false;
       }
     }
-    if (identity == null) {
-      return false;
-    }
+
     MembershipEntry membership = null;
     if (permissionExpression.contains(":")) {
       String[] permissionExpressionParts = permissionExpression.split(":");
