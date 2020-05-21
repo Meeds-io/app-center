f4398fdc802b6aaf895066fcc26b32c58860aca0
Moez Diha <38471028+mdiha@users.noreply.github.com>
2020-05-21 16:42:22 +0100

    AppCenter: Fix-User_UI_US07: Fix display on mobile device. (#22)

--- app-center-webapps/src/main/webapp/vue-apps/userSetup/components/UserSetup.vue
+++ app-center-webapps/src/main/webapp/vue-apps/userSetup/components/UserSetup.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="userApplications">
     <v-row dense>
-      <v-col>
+      <v-col xl="6">
         <user-authorizedApplications />
       </v-col>
-      <v-col cols="4">
+      <v-col sm="3" xl="6">
         <user-favoriteApplications />
       </v-col>      
     </v-row>
