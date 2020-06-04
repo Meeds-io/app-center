b6dbd6b738092b2b3842740fec3fc071ece8caec
Moez Diha <38471028+mdiha@users.noreply.github.com>
2020-06-04 10:13:34 +0100

    AppCenter: Fix-User_Settings_US01: Make sure to not consider mandatory apps for counting maximum fav apps. (#48)

--- app-center-webapps/src/main/webapp/vue-apps/userSetup/components/UserAuthorizedApplications.vue
+++ app-center-webapps/src/main/webapp/vue-apps/userSetup/components/UserAuthorizedApplications.vue
@@ -145,12 +145,6 @@ export default {
       authorizedApplicationsListMsg: this.$t('appCenter.userSetup.loading')
     };
   },
-  computed: {
-    canAddFavorite() {
-      const countFavoriteApps = this.authorizedApplicationsList.filter(app => !app.byDefault && app.favorite).length;
-      return countFavoriteApps < this.maxFavoriteApps;
-    }
-  },
   watch: {
     searchText() {
       if (this.searchText && this.searchText.trim().length) {
@@ -194,7 +188,6 @@ export default {
           } else {
             this.showPaginator = false;
           }
-          this.$emit('canAddFavorite', this.canAddFavorite);
         });
     },
     addOrDeleteFavoriteApplication(application) {
@@ -210,7 +203,6 @@ export default {
           if (!application.favorite) {
             this.$parent.$children[1].deleteFavoriteApplication(application.id);
           }
-          this.$emit('canAddFavorite', this.canAddFavorite);
         });
     },
     nextPage() {
