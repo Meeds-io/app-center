edc758964fd33ebbc87c4cf1b75eb5a9f001e09d
Moez Diha <38471028+mdiha@users.noreply.github.com>
2020-06-10 19:01:31 +0100

    AppCenter: Admin_UI_US04: Align application settings page layout to the design. (#61)
    * AppCenter: Admin_UI_US04: Align application settings page layout to the design.
    * AppCenter: Admin_UI_US04: Fix sonar warnings.
    * AppCenter: Admin_UI_US04: Fix display of application settings page.

--- app-center-webapps/src/main/webapp/vue-apps/adminSetup/components/AdminSetup.vue
+++ app-center-webapps/src/main/webapp/vue-apps/adminSetup/components/AdminSetup.vue
@@ -16,36 +16,28 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 -->
 <template>
   <v-app class="applicationsAdmin">
-    <div class="uiTabNormal uiTabInPage white">
-      <ul class="nav nav-tabs px-0">
-        <li :class="{ active: activeTab === 1 }" @click="activeTab = 1">
-          <a href="#globalParams" data-toggle="tab">{{
-            $t("appCenter.adminSetupForm.generalSettings")
-          }}</a>
-        </li>
-        <li :class="{ active: activeTab === 2 }" @click="activeTab = 2">
-          <a href="#list" data-toggle="tab">{{
-            $t("appCenter.adminSetupForm.applications")
-          }}</a>
-        </li>
-      </ul>
-      <div class="px-3 pb-2">
-        <div
-          v-if="activeTab === 1"
-          id="globalParams"
-          class="tab-pane fade in active"
+    <v-row class="uiTabNormal uiTabInPage white">
+      <v-col>
+        <v-tabs
+          class="appAdminTabs"
+          background-color="transparent"
         >
-          <adminSetup-generalParams />
-        </div>
-        <div
-          v-if="activeTab === 2"
-          id="list"
-          class="tab-pane fade in active"
-        >
-          <adminSetup-list :page-size="pageSize" />
-        </div>
-      </div>
-    </div>
+          <v-tab>
+            {{ $t('appCenter.adminSetupForm.applications') }}
+          </v-tab>
+          <v-tab>
+            {{ $t('appCenter.adminSetupForm.generalSettings') }}
+          </v-tab>
+
+          <v-tab-item class="px-4 py-2">
+            <adminSetup-list :page-size="pageSize" />
+          </v-tab-item>
+          <v-tab-item class="px-4 py-2">
+            <adminSetup-generalParams />
+          </v-tab-item>
+        </v-tabs>        
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 
@@ -53,20 +45,11 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 export default {
   data() {
     return {
-      activeTab: 1,
       pageSize: 10,
     };
   },
   created() {
     this.pageSize = this.$parent.$data.preferences.pageSize && Number(this.$parent.$data.preferences.pageSize) || this.pageSize;
   },
-  mounted() {
-    const windowLocationHash = window.location.hash;
-    if (windowLocationHash === '#list') {
-      this.activeTab = 2;
-    } else {
-      this.activeTab = 1;
-    }
-  }
 };
 </script>
