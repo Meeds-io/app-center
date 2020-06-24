753a997fe28f2837fd78cc3882d3332a54751e5e
Moez Diha <mdiha@exoplatform.com>
2020-06-24 10:29:44 +0100

    AppCenter: EditApplication: Make sure to display the appropriate app image in application center and in app launcher.

--- app-center-webapps/src/main/webapp/vue-apps/userSetup/components/UserSetup.vue
+++ app-center-webapps/src/main/webapp/vue-apps/userSetup/components/UserSetup.vue
@@ -18,10 +18,10 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
   <div class="userApplications">
     <v-row dense>
       <v-col class="authorizedApplicationsContainer">
-        <user-authorizedApplications :can-add-favorite="canAddFavorite"></user-authorizedApplications>
+        <user-authorizedApplications :can-add-favorite="canAddFavorite" :default-app-image="defaultAppImage"></user-authorizedApplications>
       </v-col>
       <v-col class="userFavoriteApplicationsContainer" sm="3">
-        <user-favoriteApplications @canAddFavorite="setCanAddFavorite"></user-favoriteApplications>
+        <user-favoriteApplications :default-app-image="defaultAppImage" @canAddFavorite="setCanAddFavorite"></user-favoriteApplications>
       </v-col>      
     </v-row>
   </div>
@@ -31,15 +31,38 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 export default {
   data() {
     return {
+      defaultAppImage: {
+        fileBody: '',
+        fileName: '',
+        invalidSize: false,
+        invalidImage: false
+      },
       canAddFavorite: false,
     };
   },
   created() {
+    this.getAppGeneralSettings();
     this.pageSize = this.$parent.$data.preferences.pageSize;
   },
   methods: {
     setCanAddFavorite(canAddFavorite) {
       this.canAddFavorite = canAddFavorite;
+    },
+    getAppGeneralSettings() {
+      return fetch('/portal/rest/app-center/settings', {
+        method: 'GET',
+        credentials: 'include',
+      })
+        .then(resp => {
+          if (resp && resp.ok) {
+            return resp.json();
+          } else {
+            throw new Error('Error getting favorite applications list');
+          }
+        })
+        .then(data => {
+          Object.assign(this.defaultAppImage, data && data.defaultApplicationImage);
+        });
     },
   },
 };
