d48b63a084ae4acb3807371d9b0711d694601784
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2021-01-26 09:56:33 +0100

    MAINT-37593 : handle AppCenter NPE with lost apps images (#184)
    * handle AppCenter NPE with lost apps images
    
    improvement
    
    update
    
    update 2
    
    update 3
    
    update 4
    
    update unit tests
    
    update
    
    update 2
    
    revert test unit changes
    
    update
    
    update
    
    * Update app-center-webapps/src/main/webapp/vue-apps/userSetup/components/UserFavoriteApplications.vue
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>

--- app-center-webapps/src/main/webapp/vue-apps/appLauncher/components/AppLauncherDrawer.vue
+++ app-center-webapps/src/main/webapp/vue-apps/appLauncher/components/AppLauncherDrawer.vue
@@ -73,7 +73,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
                   :href="application.computedUrl"
                   @click="logOpenApplication(application.id)"
                 >
-                  <img v-if="application.imageFileId" class="appLauncherImage" :src="`/portal/rest/app-center/applications/illustration/${application.id}`" />
+                  <img v-if="application.imageFileId && application.imageFileName" class="appLauncherImage" :src="`/portal/rest/app-center/applications/illustration/${application.id}`" />
                   <img v-else-if="defaultAppImage.fileBody" class="appLauncherImage" :src="`/portal/rest/app-center/applications/illustration/${application.id}`" />
                   <img v-else class="appLauncherImage" src="/app-center/skin/images/defaultApp.png" />
                   <span
@@ -108,7 +108,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
                   :href="application.computedUrl"
                   @click="logOpenApplication(application.id)"
                 >
-                  <img v-if="application.imageFileId" class="appLauncherImage" :src="`/portal/rest/app-center/applications/illustration/${application.id}`" />
+                  <img v-if="application.imageFileId && application.imageFileName" class="appLauncherImage" :src="`/portal/rest/app-center/applications/illustration/${application.id}`" />
                   <img v-else-if="defaultAppImage.fileBody" class="appLauncherImage" :src="`/portal/rest/app-center/applications/illustration/${application.id}`" />
                   <img v-else class="appLauncherImage" src="/app-center/skin/images/defaultApp.png" />
                   <span 
