f4a2dce936df2d401164cb4bd5c3c191acb9ceb4
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-06-11 09:00:46 +0100

    feat: Apply Application Background Color Class - MEED-7042 - Meeds-io/MIPs#143

--- app-center-webapps/src/main/webapp/vue-apps/adminSetup/components/AdminSetup.vue
+++ app-center-webapps/src/main/webapp/vue-apps/adminSetup/components/AdminSetup.vue
@@ -24,7 +24,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
           <v-tabs
             v-model="selectedTab"
             slider-size="4"
-            class="card-border-radius overflow-hidden">
+            class="card-border-radius overflow-hidden app-background-color">
             <v-tab
               key="applications"
               href="#applications"
@@ -36,7 +36,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
             </v-tab>
           </v-tabs>
 
-          <v-tabs-items v-model="selectedTab" class="mt-2 card-border-radius  overflow-hidden">
+          <v-tabs-items v-model="selectedTab" class="mt-2 card-border-radius app-background-color overflow-hidden">
             <v-tab-item
               id="applications"
               value="applications"
