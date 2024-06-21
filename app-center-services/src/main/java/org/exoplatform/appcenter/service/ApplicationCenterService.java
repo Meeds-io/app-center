29e31321299dac13e23b42196fac937f25c7af30
Jihed Chabbeh <31882754+Jihed525@users.noreply.github.com>
2024-06-21 16:14:10 +0100

    fix: New app not created it its url contains special chars - EXO-72513 - Meeds-io/meeds#2216
    Before this change, when open applications center and add an application and in url input add an url, application not saved but updating an already existing app, the url is well saved. After this change, application is saved and displayed in app list.
    
    (cherry picked from commit 69bb3df)

--- app-center-services/src/main/java/org/exoplatform/appcenter/service/ApplicationCenterService.java
+++ app-center-services/src/main/java/org/exoplatform/appcenter/service/ApplicationCenterService.java
@@ -819,7 +819,7 @@ public class ApplicationCenterService implements Startable {
 
   private boolean isUrlValid(String url) {
     //[-a-zA-Z0-9@:%._\\\\/+~#=] allowed characters
-    String regex = "(http(s)?:\\/\\/.)[-a-zA-Z0-9@:%._\\\\/+~#=]{2,256}";
+    String regex = "(http(s)?:\\/\\/.)[-a-zA-Z0-9@:%._\\\\/+~#=?&]{2,256}";
     Pattern pattern = Pattern.compile(regex);
     return url != null && !url.isBlank() && (url.startsWith("/portal/") || url.startsWith("./") || pattern.matcher(url).matches());
   }
