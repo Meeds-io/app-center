c5421bf6324bcaf0fa6702656eeb68961a17f01a
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-03-24 12:30:14 +0100

    feat: Add myApplications portlet to general tools category - MEED-8653 - Meeds-io/MIPs#183 (#385)
    Add myApplications portlet to general tools category

---
+++ app-center-webapps/src/main/webapp/vue-apps/myApplications/components/MyApplicationsApp.vue
@@ -0,0 +1,41 @@
+<!--
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+
+<template>
+  <v-app>
+    <v-card
+      class="application-body position-static pa-5"
+      flat>
+      <div>
+        <h4 class="text-header mt-0 ms-auto">
+          {{ $t('myApplications.name.label') }}
+        </h4>
+      </div>
+      <span class="align-center ma-auto">
+        Not yet implemented
+      </span>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+};
+</script>
