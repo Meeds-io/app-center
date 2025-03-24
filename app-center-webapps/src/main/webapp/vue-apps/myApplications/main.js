0912c6ce9e2491f3cbeb717aa29b618e68bbae05
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-03-24 12:30:14 +0100

    feat: Add myApplications portlet to general tools category - MEED-8653 - Meeds-io/MIPs#183 (#385)
    Add myApplications portlet to general tools category

--- app-center-webapps/src/main/webapp/vue-apps/myApplications/main.js
+++ app-center-webapps/src/main/webapp/vue-apps/myApplications/main.js
@@ -1,32 +1,48 @@
 /*
- * This file is part of the Meeds project (https://meeds.io/).
- * Copyright (C) 2020 Meeds Association
- * contact@meeds.io
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-import MyApplicationsApp from './components/MyApplications.vue';
+* This file is part of the Meeds project (https://meeds.io/).
+*
+* Copyright (C) 2025 Meeds Association contact@meeds.io
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
+import './initComponents.js';
 
-//should expose the locale ressources as REST API
-const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language || 'en';
-const url = `/app-center/i18n/locale.addon.appcenter?lang=${lang}`;
+// get overridden components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('MyApplications');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+const lang = eXo && eXo.env.portal.language || 'en';
+const url = `/app-center/i18n/locale.portlet.MyApplications?lang=${lang}`;
+const vuetify = Vue.prototype.vuetifyOptions;
+const appId = 'myApplications';
 
 export function init() {
+  console.log('hello world');
   exoi18n.loadLanguageAsync(lang, url).then(i18n => {
     Vue.createApp({
-      render: h => h(MyApplicationsApp),
-      i18n,
-      vuetify: Vue.prototype.vuetifyOptions,
-    }, '#myApplications', 'My Application Center');
+      template: '<my-applications-app />',
+      vuetify,
+      i18n
+    }, `#${appId}`, 'My Application Center');
   });
 }
+
