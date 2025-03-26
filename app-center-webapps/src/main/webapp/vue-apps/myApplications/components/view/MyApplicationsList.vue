8793772f59a64cfaa29204df1df7a35458a96c96
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-03-26 09:48:45 +0100

    feat: Implement application list for myApplications portlet - MEED-8563, MEED-8562 - Meeds-io/MIPs#183 (#388)
    Implement application list for myApplications portlet to manage
    application display and darg/drop application order

--- app-center-webapps/src/main/webapp/vue-apps/myApplications/components/view/MyApplicationsList.vue
+++ app-center-webapps/src/main/webapp/vue-apps/myApplications/components/view/MyApplicationsList.vue
@@ -20,7 +20,15 @@
 
 <template>
   <div
-    v-if="!applicationsList.length"
+    v-if="isLoading"
+    class="d-flex flex-column justify-center align-center flex-grow-1">
+    <v-progress-circular
+      class="ma-auto"
+      color="primary"
+      indeterminate />
+  </div>
+  <div
+    v-else-if="!applicationsList.length"
     class="d-flex flex-column justify-center align-center flex-grow-1">
     <v-icon size="60" class="secondary--text mb-2">
       fas fa-th
@@ -29,6 +37,26 @@
       {{ $t('myApplications.add.application.label') }}
     </p>
   </div>
+  <v-layout
+    v-else>
+    <component
+      :is="isMobile && 'div' || 'draggable'"
+      v-model="applicationsList"
+      class="d-flex flex-wrap justify-start"
+      @end="onDragEnd">
+      <my-application-item
+        v-for="(application, index) in applicationsList"
+        :key="application.id"
+        :position="index"
+        :application="application"
+        :default-app-image="defaultAppImage"
+        :width="150"
+        :maxWidth="150"
+        :minHeight="150"
+        :maxHeight="150"
+        class="d-flex ma-2 flex-grow-1" />
+    </component>
+  </v-layout>
 </template>
 
 <script>
@@ -37,6 +65,24 @@ export default {
     applicationsList: {
       type: Array,
       default: () => []
+    },
+    defaultAppImage: {
+      type: Object,
+      default: null
+    },
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    }
+  },
+  methods: {
+    onDragEnd() {
+      this.$emit('list-updated', this.applicationsList);
     }
   }
 };
