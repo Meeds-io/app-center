1cbc292053c38c17438f36e698401ef5c14b9a6e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-04-08 12:57:42 +0100

    feat: Add Post Activity Drawer default configuration - MEED-8612 - Meeds-io/MIPs#185 (#400)
    This change will add a default Application to access Activity Drawer
    from App Center.

--- app-center-services/src/main/java/io/meeds/appcenter/model/ApplicationForm.java
+++ app-center-services/src/main/java/io/meeds/appcenter/model/ApplicationForm.java
@@ -22,10 +22,12 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.EqualsAndHashCode.Exclude;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class ApplicationForm extends Application {
 
   @Exclude
