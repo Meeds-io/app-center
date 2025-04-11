a81aeb91ec2e74f5f6abb164c1c52ad0c6ff3d79
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-04-11 11:38:21 +0100

    feat: Add Option in Application to open url in same tab - MEED-8788 - Meeds-io/MIPs#185 (#429)
    This change will allow to manually choose the target of app link.

--- app-center-services/src/test/java/io/meeds/appcenter/service/ApplicationCenterInjectServiceTest.java
+++ app-center-services/src/test/java/io/meeds/appcenter/service/ApplicationCenterInjectServiceTest.java
@@ -157,6 +157,7 @@ public class ApplicationCenterInjectServiceTest {
     return new Application(id,
                            TITLE,
                            URL,
+                           true,
                            HELP_PAGE_URL,
                            DESCRIPTION,
                            SHORTCUT,
