cb76375711cd830f97fc958241666b5739e291f2
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-04-16 14:25:57 +0100

    feat: Add Topbar extension - MEED-8815 - Meeds-io/MIPs#185 (#445)
    This change will allow to add applications from app-center extension as Topbar application.

--- app-center-webapps/src/main/webapp/vue-apps/application-launcher/main.js
+++ app-center-webapps/src/main/webapp/vue-apps/application-launcher/main.js
@@ -16,7 +16,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import './initComponents.js';
-import '../application-common/initComponents.js';
 
 //should expose the locale ressources as REST API
 const appId = 'appLauncher';
