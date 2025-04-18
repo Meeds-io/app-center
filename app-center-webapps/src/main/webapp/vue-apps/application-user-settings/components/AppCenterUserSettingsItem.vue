a4e153e57fdc718764cb7cdb91bad25ca28413aa
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-04-18 09:24:47 +0100

    feat: Add App Center Shortcuts Listing in User Settings - MEED-8637 - Meeds-io/MIPs#185 (#450)

---
+++ app-center-webapps/src/main/webapp/vue-apps/application-user-settings/components/AppCenterUserSettingsItem.vue
@@ -0,0 +1,80 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <div class="d-flex flex-column flex-md-row align-center">
+    <v-card
+      class="d-flex align-center align-self-start mb-2 mb-md-0"
+      min-width="fit-content"
+      width="380"
+      max-width="30%"
+      flat>
+      <v-avatar
+        class="d-flex align-center justify-center flex-grow-0 flex-shrink-0 me-3"
+        size="22"
+        tile>
+        <v-img
+          v-if="application.imageUrl"
+          :src="application.imageUrl"
+          max-height="20"
+          max-width="20"
+          contain />
+        <v-icon
+          v-else-if="application.icon"
+          size="20"
+          class="d-flex align-center justify-center">
+          {{ application.icon }}
+        </v-icon>
+      </v-avatar>
+      {{ application.title }}
+    </v-card>
+    <div class="d-flex align-md-center align-self-end mb-2 mb-md-0">
+      <v-card
+        class="fill-height grey-lighten1-background white--text px-5 py-1"
+        flat>
+        {{ $t('appCenter.adminSetupForm.ctrl') }}
+      </v-card>
+      <v-icon class="mx-2" size="24">fa-plus</v-icon>
+      <v-card
+        class="fill-height grey-lighten1-background white--text px-5 py-1"
+        flat>
+        {{ $t('appCenter.adminSetupForm.shift') }}
+      </v-card>
+      <v-icon class="mx-2" size="24">fa-plus</v-icon>
+      <v-card
+        class="fill-height grey-lighten1-background white--text px-5 py-1"
+        width="10"
+        flat>
+        {{ application.shortcut }}
+      </v-card>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    application: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
