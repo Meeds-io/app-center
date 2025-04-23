369f3a72ce7f1bdee3b490f585bb2c664dbd3988
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-04-23 09:22:06 +0100

    feat: Allow to translate Apps descriptions in AppCenter - MEED-8878 - Meeds-io/MIPs#185

--- app-center-webapps/src/main/webapp/vue-apps/topbar-pinned-applications/main.js
+++ app-center-webapps/src/main/webapp/vue-apps/topbar-pinned-applications/main.js
@@ -73,6 +73,9 @@ export async function init(pinnedApplicationIds) {
               const title = /\s/.test(app.title) ? app.title.replace(/ /g,'.').toLowerCase() : app.title.toLowerCase();
               if (this.$te(`appCenter.system.application.${title}`)) {
                 app.title = this.$t(`appCenter.system.application.${title}`);
+                if (this.$te(`appCenter.system.application.${title}.description`) && !app.description?.length) {
+                  app.description = this.$t(`appCenter.system.application.${title}.description`);
+                }
               }
             }
             if (app.type === 'LINK' && app.url) {
