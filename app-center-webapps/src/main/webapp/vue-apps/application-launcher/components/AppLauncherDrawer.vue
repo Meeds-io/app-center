d4a0d23ed531a3308618ca536100ed3d5a731d32
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-10-24 11:52:03 +0100

    feat: Allow to disabled App By Quick Action Extension

--- app-center-webapps/src/main/webapp/vue-apps/application-launcher/components/AppLauncherDrawer.vue
+++ app-center-webapps/src/main/webapp/vue-apps/application-launcher/components/AppLauncherDrawer.vue
@@ -340,7 +340,7 @@ export default {
       this.loading = true;
       return this.getApplications(isFavoriteFilter)
         .then(data => {
-          const applications = data.applications;
+          const applications = data.applications?.filter(app => app.type !== 'DRAWER' || !this.$root.quickActions[app.url]?.enabled || this.$root.quickActions[app.url].enabled());
           // manage system apps localized names
           applications.forEach(app => {
             if (app.system) {
